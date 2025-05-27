50fd61cc17b16a1512f8e392cef832e686044e32
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2025-05-27 15:40:15 -0300

    sobre adicionado

--- src/components/Sobre/index.tsx
+++ src/components/Sobre/index.tsx
@@ -2,27 +2,28 @@
 import { AnimatedSection, AnimatedText, AnimatedImage } from "../ui/animated-section"
 export default function Sobre() {
     return (
-        <AnimatedSection className="min-h-screen w-full flex flex-col items-center justify-center" id="somos">
+        <AnimatedSection className="mt-10 lg:p-20  w-[90%] flex flex-col items-center justify-center" id="somos">
             <AnimatedText className="font-bold text-2xl lg:text-3xl text-blue-950 mb-10" delay={0.2}>
                 Quem Somos?
             </AnimatedText>
-            <div className="lg:flex justify-center w-[90%] max-w-[950px] rounded-lg bg-blue-950 items-center shadow-lg">
+            <div className="flex flex-col lg:flex-row w-[100%]">
                 <AnimatedImage className="flex items-center justify-center" delay={0.4}>
                     <img
-                        src="./Sobre.png"
+                        src="./human.png"
                         alt="Imagem ilustrativa da equipe ou serviços da MRE Soluções"
-                        className="w-full h-[300px] rounded-lg object-cover"
+                        className="w-[450px] max-w-[650px] rounded-lg object-cover"
                         loading="lazy"
                     />
                 </AnimatedImage>
-                <AnimatedText className="flex flex-col text-justify max-w-[650px] p-6" delay={0.6}>
-                    <p className="text-[19px] lg:text-[24px] text-white text-justify">
-                        Somos uma casa de softwares especializada em criar soluções digitais rápidas, eficientes e sob medida.
-                        Atendemos desde pessoas físicas que desejam um portfólio profissional até empresas que buscam landing pages, sites institucionais ou sistemas leves.
-                        Nosso foco é tirar a complexidade da tecnologia do seu caminho.
+                <AnimatedText className="flex flex-col text-justify max-w-[650px]" delay={0.6}>
+                    <p className="mt-5 text-[19px] lg:text-[24px] text-justify">
+
+                        Na MRE, acreditamos que uma landing page eficaz é aquela que combina estética, funcionalidade e estratégia. Por isso, nosso time é formado por profissionais experientes em desenvolvimento web, comprometidos em entregar soluções que realmente geram resultados.
+                        <br></br><br></br>Cada projeto é desenvolvido com atenção aos detalhes, desde a escolha das cores e tipografia até a estrutura de conteúdo e otimização para conversão.
                     </p>
                 </AnimatedText>
             </div>
+
         </AnimatedSection>
     )
 }
